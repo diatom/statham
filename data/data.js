c5f59964cebb5940d131600a943f4e119db53ca5
Severin <Supriore@gmail.com>
2025-03-10 16:03:01 +0300

    update footer

--- data/data.js
+++ data/data.js
@@ -1,5 +1,5 @@
 export const contact = [
     {'По новым идеям пишите мне в Телеграм: @Severinz': `https://telegram.me/Severinz`},
     {'Читайте блог: sirseverin.ru': `https://sirseverin.ru/`},
-    {'Пейте Ибри': `https://drinkibri.ru/`},
+    {'Стетхем одобряет напитки Ибри drinkibri.ru': `https://drinkibri.ru/`},
 ]
