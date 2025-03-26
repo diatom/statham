d44b6a7f52644bffb19408642ea0236144657206
Severin <Supriore@gmail.com>
2025-03-26 11:54:50 +0300

    update name of donate link

--- site.mjs
+++ site.mjs
@@ -99,7 +99,7 @@ class PageIndex extends Page {
           E.div.props({class: `greeting`}).chi(
             E.h1.chi(`Джейсон Стетхем — это великий русский поэт, а здесь его цитаты`),
             E.span.chi(`Последнее обновление: 25.03.2025`),
-            E.a.props({href: `https://pay.cloudtips.ru/p/61bbe89e`, class: `donate`}).chi(`Подкинуть ₽ Стетхему на стоник`),
+            E.a.props({href: `https://pay.cloudtips.ru/p/61bbe89e`, class: `donate`}).chi(`Подкинуть донаты Стетхему на новую парку`),
             E.h3.chi(`Цитата дня`),
             E.div.props({class: `today-quote`}).chi(``),
           ),
